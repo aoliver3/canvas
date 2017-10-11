e8307f2214f0bc0a9f6285d2c41b164b8aae260d
Anthony <anthonyoliver84@comcast.net>
2017-10-10 21:52:25 -0400

    added reset functionality

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ResetService } from '../reset.service';
 import { TileComponent } from '../tile/tile.component';
+import { ColorSelectionService } from '../color-selection.service';
 
 @Component({
   selector: 'app-canvas',
@@ -9,16 +9,25 @@ import { TileComponent } from '../tile/tile.component';
 })
 export class CanvasComponent implements OnInit {
 
-  private tileCanvas: Array<string> = [];
+  private tileCanvas: Array<TileComponent> = [];
 
-  constructor(private resetService: ResetService) { }
+  constructor(private colorSelectionService: ColorSelectionService) { }
 
   ngOnInit() {
     this.generateTileCanvas();
   }
 
   private generateTileCanvas(): void {
-    this.tileCanvas = this.resetService.resetTileCanvas(100);
+    this.tileCanvas = this.resetTileCanvas(100); //tiles at app start
+  }
+
+  private resetTileCanvas(numberOfTiles: number): Array<TileComponent> {
+    let tileCanvas: Array<TileComponent> = [];
+    for (let i=0; i<numberOfTiles; i++) { 
+      let tile = new TileComponent(this.colorSelectionService);
+      tileCanvas.push(tile);
+    }
+    return tileCanvas;
   }
 
 }
